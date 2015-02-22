d1e18070a6b40f866b503690ce73a9d10e990f26
Rob Paskin <rjpaskin+git@gmail.com>
2015-02-22 23:57:09 +0000

    Updating to 4078047baf9c8012b84fe1c0b524b98457cecc7f.

--- assets/custom.js
+++ assets/custom.js
@@ -7,16 +7,30 @@
 // - Browser scroll position was lost due to fragment navigation
 //   (Worked around this by converting #id values to #!id values before navigating.)
 $(document).ready(function () {
- 
+
     if (location.hash.substr(0,2) == "#!") {
         $("a[href='#" + location.hash.substr(2) + "']").tab("show");
     }
- 
+
     $("a[data-toggle='tab']").on("shown", function (e) {
         var hash = $(e.target).attr("href");
         if (hash.substr(0,1) == "#") {
             location.replace("#!" + hash.substr(1));
         }
     });
- 
+
+
+    // Temporary workaround for Maruku double-escaping HTML entities in <code> elements
+    $(".exercise").find("code").each(function() {
+      var $codeBlock = $(this);
+
+
+      // Don't do replacements on syntax-highlighted blocks
+      if ($codeBlock.attr("class") === undefined) {
+        $codeBlock.text(function(_, text) {
+          return text.replace(/&#39;/g, "'");
+        })
+      }
+    });
+
 });
